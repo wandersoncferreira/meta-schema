fd10305c8c4b39d40e42378d751e6b116d701210
Wanderson Ferreira <iagwanderson@gmail.com>
2020-01-20 21:35:05 -0300

    improve tests and documentation

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject meta-schema "0.1.0-SNAPSHOT"
+(defproject meta-schema "0.1.1"
   :description "DSL to write data processing schemas faster"
   :url "https://github.com/wandersoncferreira/meta-schema"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [metosin/spec-tools "0.10.1"]]
+                 [metosin/spec-tools "0.10.1"]
+                 [org.clojure/test.check "0.10.0"]]
   :repl-options {:init-ns meta-schema.core})
--- test/meta_schema/core_test.clj
+++ test/meta_schema/core_test.clj
@@ -2,13 +2,43 @@
   (:require [clojure.test :refer :all]
             [meta-schema.core :as ms]
             [clojure.java.io :as io]
-            [clojure.spec.alpha :as s]))
+            [clojure.spec.alpha :as s]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.properties :as prop]))
+
+(def spec-gen (gen/hash-map
+               :spec (gen/elements [:zipcode :money :life :address])))
+
+(def optional-gen (gen/hash-map
+                   :optional? gen/boolean))
+
+(def nullable-gen (gen/hash-map
+                   :nullable? gen/boolean))
+
+
+(def leaf-spec (gen/let [specs spec-gen
+                         options (gen/frequency [[6 optional-gen]
+                                                 [4 (gen/return {})]])
+                         nulls (gen/frequency [[6 nullable-gen]
+                                               [4 (gen/return {})]])]
+                 (merge specs options nulls)))
+
+(def spec-file-gen (gen/hash-map
+                    :spec-name gen/keyword-ns
+                    :tk1 (gen/not-empty (gen/vector leaf-spec))
+                    :tk2 leaf-spec))
+
+(gen/sample spec-file-gen 2)
+
 
 (s/def ::money (s/or :int int?
                      :float float?
                      :double double?))
 
 (s/def ::zipcode string?)
+(s/def ::life (s/and int? #(= 42 %)))
+(s/def ::address string?)
 
 (deftest simple-file-spec
   (ms/setup! (-> (io/resource "specs")
@@ -23,3 +53,11 @@
                           :amount 20}))
     (is (not (s/valid? parser {:cep 10
                                :amount 20})))))
+
+
+(defspec cannot-break-to-nested-fns 400
+  (prop/for-all [file-spec spec-file-gen]
+                (ms/setup! (-> (io/resource "specs")
+                               (io/file)
+                               (file-seq)))
+                (= (spec-tools.core/spec? (ms/create-parser file-spec)))))
