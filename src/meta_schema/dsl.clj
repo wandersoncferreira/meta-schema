373c1534b036c1ab1a9d2819faeefe83826bbf7c
Wanderson Ferreira <iagwanderson@gmail.com>
2020-01-29 08:36:40 -0300

    more spec to describe the language itself

--- src/meta_schema/dsl.clj
+++ src/meta_schema/dsl.clj
@@ -1,17 +1,18 @@
 (ns meta-schema.dsl
   (:require [clojure.spec.alpha :as s]))
 
-
 (s/def ::spec keyword?)
 (s/def ::optional? boolean?)
 (s/def ::nullable? boolean?)
+(s/def ::destination keyword?)
 
 (s/def ::options (s/keys :req-un [::spec]
                          :opt-un [::optional?
-                                  ::nullable?]))
+                                  ::nullable?
+                                  ::destination]))
 
-
-(s/def ::location qualified-keyword?)(s/def ::intent string?)
+(s/def ::location qualified-keyword?)
+(s/def ::intent string?)
 
 (s/def ::setup-file (s/keys :req-un [::location]
                             :opt-un [::intent]))
